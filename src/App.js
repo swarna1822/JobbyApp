3430dc9d8a97553ad62abdf4d64d76f583fd47a5
swarna1822 <146362837+swarna1822@users.noreply.github.com>
2024-02-11 12:44:29 +0530

    Home Component Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+//import {Switch, Route, Redirect} from 'react-router-dom'
 import LoginComponent from './components/Login/LoginComponent';
-import HeaderComponent from './components/Header/HeaderComponent';
+//import HeaderComponent from './components/Header/HeaderComponent';
+//import HomeComponent from './components/Home/HomeComponent';
+//import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import './App.css';
 
 
 function App() {
-  return (
-    <>
-    <HeaderComponent/>
-       <LoginComponent/>
-    </>
-  );
+  // <Switch>
+  //   <Route exact path="/login" component={LoginComponent} />
+  //   <ProtectedRoute exact path="/" component={HomeComponent} />
+  //   {/* <ProtectedRoute exact path="/jobs" component={Jobs} />
+  //   <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+  //   <Route exact path="/not-found" component={NotFound} />
+  //   <Redirect to="/not-found" /> */}
+  // </Switch>
+  return(
+    <LoginComponent/>
+  )
 }
 
 export default App;
